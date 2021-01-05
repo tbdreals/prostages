ef3f3f2f0e5e2b6611b818f2a76e11a0ab4c5812
Tanguy Boscals de Réals <tbdreals@iutbayonne.univ-pau.fr>
2021-01-05 17:48:16 +0100

    Ajout de commentaires

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,11 +13,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        // On va utiliser l'outil faker afin de générer du contenu
         $faker = \Faker\Factory::create('fr_FR');
 
         /*******************************
         *** CREATION DES ENTREPRISES ***
         *******************************/
+        // Les entreprises sont créées manuellement
         $total = new Entreprise ();
         $total -> setNom("Total");
         $total -> setAdresse("10 bis rue des Cervoises, 64000 PAU");
@@ -33,8 +35,10 @@ class AppFixtures extends Fixture
         $ArcadeAndCo -> setAdresse("2 allée du Parc Montaury, 64600 ANGLET");
         $ArcadeAndCo -> setSiteWeb("https://www.arcadeandco.fr");
 
+        // On stocke les entreprises dans un tableau
         $tableauEntreprises = array($total, $safran, $ArcadeAndCo);
 
+        // on applique la méthode persist à chaque élément du tableau
         foreach ($tableauEntreprises as $entreprise) {
           $manager -> persist($entreprise);
         }
@@ -43,38 +47,48 @@ class AppFixtures extends Fixture
         /****************************************************
         *** CREATION DES FORMATIONS ET DE STAGES ASSOCIES ***
         ****************************************************/
+        // Le nombre de formations désirées
         $nbFormations = 10;
 
+        // Les formations sont générées automatiquement
         for ($i=1; $i < $nbFormations; $i++) {
+          // On commence par renseigner les caractéristiques de la formation
           $formation = new Formation();
           $formation->setIntitule($faker->sentence($nbWords = 3, $variableNbWords = true));
           $formation->setNiveau($faker->regexify('Bac \+'.'[1-8]'));
           $formation->setVille($faker->city());
           $manager->persist($formation);
 
+          // On génère un nombre aléatoire de stages à générer pour chaque formation
           $nbStagesAGenerer = $faker -> numberBetween($min = 0, $max = 7);
+
+          // Puis on génère automatiquement chacun de ces stages
           for ($numStage = 0; $numStage < $nbStagesAGenerer; $numStage++) {
+            // En remplissant ses caractéristiques
             $stage = new Stage();
             $stage -> setIntitule($faker -> sentence($nbWords = 3, $variableNbWords = true));
             $stage -> setDescription($faker -> realText($MaxNbChars = 200, $indexSize = 2));
-
             $stage -> setDateDebut($faker -> dateTimeBetween ($startDate = 'now', $endDate = '+6 months', $timezone = 'Europe/Paris'));
             $stage -> setDuree($faker -> numberBetween($min = 30, $max = 180));
             $stage -> setCompetenceRequise($faker -> realText($MaxNbChars = 100, $indexSize = 2));
             $stage -> setEmailEntreprise($faker -> email());
 
+            // On ajoute le stage à la formation
             $stage -> addFormation($formation);
 
+            // On génère un nombre aléatoire afin de choisir quelle entreprise proposera ce stage
             $numEntreprise = $faker -> numberBetween($min = 0, $max = 2);
+
+            // On ajoute l'entreprise choisie au stage
             $stage -> setEntreprise($tableauEntreprises[$numEntreprise]);
 
+            // On ajoute le stage à l'entreprise choisie
             $tableauEntreprises[$numEntreprise] -> addStage($stage);
 
             $manager -> persist($stage);
             $manager -> persist($tableauEntreprises[$numEntreprise]);
           }
         }
-
         $manager->flush();
     }
 }
